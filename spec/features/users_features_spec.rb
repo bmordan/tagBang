2891ea02eab32076dc6ab802d56ea3c93f2978cf
Bernard Mordan <bernardmordan@gmail.com>
2014-11-08 16:26:35 +0000

    sign up in place with confirm flash message

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe 'tag bang. instagram for skaters' do
+
   it 'you can see the name of the site' do
     visit '/'
     expect(page).to have_content('TagBang')
@@ -10,4 +11,14 @@ describe 'tag bang. instagram for skaters' do
     visit '/'
     expect(page).to have_link 'Log in'
   end
+
+  it 'when you are not Logged in you dont see Log out' do
+    expect(page).not_to have_link 'Log out'
+  end
+
+  it 'so start by signing up' do
+    _signup_testuser
+    expect(page).to have_content 'You have signed up successfully'
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,3 +41,12 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+
+def _signup_testuser
+  visit '/'
+  click_link 'Sign up'
+  fill_in('Email', with: 'test@example.com')
+  fill_in('Password', with: 'password')
+  fill_in('Password confirmation', with: 'password')
+  click_button('Sign up')
+end
