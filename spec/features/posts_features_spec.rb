00ee24149ae7abdfe58886a6b0582e22cad6b1c6
Bernard Mordan <bernardmordan@gmail.com>
2014-11-09 10:20:46 +0000

    form added and initial redirect flow working with a test file attached to the form

--- spec/features/posts_features_spec.rb
+++ spec/features/posts_features_spec.rb
@@ -8,9 +8,17 @@ describe 'the core of this site is the photos' do
     expect(page).to have_content 'Welcome to TagBang. Lets start!'
   end
 
-  it 'users can add photos' do
+  it 'because you can add photos' do
     visit '/'
     _login_testuser
     expect(page).to have_css 'form'
+  end
+
+  it 'to your account' do
+    visit '/'
+    _login_testuser
+    attach_file('post[image]',File.join(Rails.root, '/spec/testImageFiles/13-percent.jpg'))
+    click_button 'Bang'
+    expect(current_path).to eq posts_path
   end
 end
