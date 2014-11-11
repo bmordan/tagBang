edb70de0b89995c8e26c9971b312c03253d5b122
Bernard Mordan <bernardmordan@gmail.com>
2014-11-11 09:21:57 +0000

    number-price field added to upload form

--- spec/features/commerce_features_spec.rb
+++ spec/features/commerce_features_spec.rb
@@ -18,4 +18,12 @@ describe 'Earn money from your photos' do
     click_link '£'
     expect(current_path).to eq '/charges/new'
   end
+
+  it 'shows you the price of the photo' do
+    visit '/'
+    _login_testuser
+    attach_file('post[image]',File.join(Rails.root, '/spec/testImageFiles/13-percent.jpg'))
+    fill_in 'price', with: '8'
+    click_button ''
+  end
 end
