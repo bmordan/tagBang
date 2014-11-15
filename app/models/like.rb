03c0c8d4046de2589bbf2f13126034999f13445d
Bernard Mordan <bernardmordan@gmail.com>
2014-11-15 23:49:05 +0000

    likes working on rails. Only works once between page refreshes

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-  validates_uniqueness_of :post_id, :scope => :user_id
+  validates_uniqueness_of :user_id, :scope => :post_id
 
   def self.this_photo(photo_id)
     self.where(post_id: photo_id).count
